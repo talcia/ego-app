742637019c8588381661527c868ddc930d9c7f5f
Natalia <59486546+talcia@users.noreply.github.com>
2024-08-28 12:22:43 +0200

    3rd party

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	webpack(config, options) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [
+				{
+					loader: 'url-loader',
+					options: {
+						limit: 8192, // Adjust the limit as needed
+						name: '[name].[hash].[ext]',
+						publicPath: '/_next/static/images/',
+						outputPath: 'static/images/',
+					},
+				},
+			],
+		});
+
+		return config;
+	},
+	reactStrictMode: true,
 };
 
 export default nextConfig;
