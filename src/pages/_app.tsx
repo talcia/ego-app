97ef7182a71088ffb7fa0503edf0fefc185562f0
Natalia <59486546+talcia@users.noreply.github.com>
2024-08-29 10:47:21 +0200

    context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app';
 import Layout from '@/components/layout/layout';
 import '@/styles/globals.css';
+import { AdminContextProvider } from '@/store/admin-context';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<AdminContextProvider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</AdminContextProvider>
 	);
 }
